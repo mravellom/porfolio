fbddf8c1fa48209549f465556395aa8d53fc5630
mravellom <miguel.ravello.madariaga@gmail.com>
2025-05-12 16:54:58 -0400

    a

---
+++ README.md
@@ -0,0 +1,2 @@
+# Porfolio
+portafolio mio
---
+++ script.js
@@ -0,0 +1,36 @@
+let menuVisible = false;
+
+function mostraOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true
+    }
+}
+
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("angular");
+        habilidades[3].classList.add("java");
+        habilidades[4].classList.add("python");
+        habilidades[5].classList.add("comunicacion");
+        habilidades[6].classList.add("trabajo");
+        habilidades[7].classList.add("creatividad");
+        habilidades[8].classList.add("dedicacion");
+        habilidades[9].classList.add("proyect");
+
+
+    }
+}
+
+window.onscroll = function(){
+    efectoHabilidades();
+
+}
